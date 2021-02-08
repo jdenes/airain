a5999da474a3af783a0e8d917048223a431d821b
Julien Denes <julien50@sfr.fr>
2021-02-08 19:58:54 +0100

    Added constants.py

---
+++ src/utils/constants.py
@@ -0,0 +1,56 @@
+"""
+For each company in the DOW, a list of keywords to filter news and make sure they are relevant.
+"""
+COMPANIES_KEYWORDS = {'AAPL': ['aap', 'apple', 'phone', 'mac', 'microsoft'],
+                      'XOM': ['xom', 'exxon', 'mobil', 'petrol', 'gas', 'energy'],
+                      'KO': ['ko', 'coca', 'cola', 'pepsi', 'soda'],
+                      'INTC': ['intc', 'intel', 'chip', 'cpu', 'computer'],
+                      'WMT': ['wmt', 'walmart', 'food'],
+                      'MSFT': ['msft', 'microsoft', 'gates', 'apple', 'computer'],
+                      'IBM': ['ibm', 'business', 'machine'],
+                      'CVX': ['cvx', 'chevron', 'petrol', 'gas', 'energy'],
+                      'JNJ': ['jnj', 'johnson', 'health', 'medi', 'pharma'],
+                      'PG': ['pg', 'procter', 'gamble', 'health', 'care'],
+                      'PFE': ['pfe', 'pfizer', 'health', 'medi', 'pharma'],
+                      'VZ': ['vz', 'verizon', 'comm'],
+                      'BA': ['ba', 'boeing', 'plane', 'air'],
+                      'MRK': ['mrk', 'merck', 'health', 'medi', 'pharma'],
+                      'CSCO': ['csco', 'cisco', 'system', 'techn'],
+                      'HD': ['hd', 'home', 'depot', 'construction'],
+                      'MCD': ['mcd', 'donald', 'food', 'burger'],
+                      'MMM': ['mmm', '3m'],
+                      'GE': ['ge', 'general', 'electric', 'tech', 'energy'],
+                      'NKE': ['nke', 'nike', 'sport', 'wear'],
+                      'CAT': ['cat', 'caterpillar', 'construction'],
+                      'V': ['visa', 'bank', 'card', 'pay'],
+                      'JPM': ['jpm', 'morgan', 'chase', 'bank'],
+                      'AXP': ['axp', 'american', 'express', 'bank', 'card', 'pay'],
+                      'GS': ['gs', 'goldman', 'sachs', 'bank'],
+                      'UNH': ['unh', 'united', 'health', 'insurance'],
+                      'TRV': ['trv', 'travel', 'insurance'],
+                      # 'UTX': ['utx', 'united', 'tech'],
+                      }
+
+"""
+List of companies used in this project (subset of DOW).
+"""
+COMPANIES = ['AAPL', 'XOM', 'KO', 'INTC', 'WMT', 'MSFT', 'IBM', 'CVX', 'JNJ', 'PG', 'PFE', 'VZ', 'BA', 'MRK',
+             'CSCO', 'HD', 'MCD', 'MMM', 'GE', 'NKE', 'CAT', 'V', 'JPM', 'AXP', 'GS', 'UNH', 'TRV']
+
+"""
+Leverages on Trading 212 CFD.
+"""
+LEVERAGES = {'AAPL': 1, 'XOM': 1, 'KO': 1, 'INTC': 1, 'WMT': 1, 'MSFT': 1, 'IBM': 1, 'CVX': 1, 'JNJ': 1,
+             'PG': 1, 'PFE': 1, 'VZ': 1, 'BA': 1, 'MRK': 1, 'CSCO': 1, 'HD': 1, 'MCD': 1, 'MMM': 1,
+             'GE': 1, 'NKE': 1, 'CAT': 1, 'V': 1, 'JPM': 1, 'AXP': 1, 'GS': 1, 'UNH': 1, 'TRV': 1}
+
+"""
+Subset of companies identifies as performers.
+"""
+PERFORMERS = ['AAPL', 'XOM', 'KO', 'INTC', 'WMT', 'MSFT', 'CVX', 'MMM', 'V', 'GS']
+
+"""
+Full list of DOW companies.
+"""
+DOW = ['AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CSCO', 'CVX', 'GS', 'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'KO', 'JPM', 'MCD',
+       'MMM', 'MRK', 'MSFT', 'NLE', 'PG', 'TRV', 'UNH', 'CRM', 'VZ', 'V', 'WBA', 'WMT', 'DIS', 'DOW']
